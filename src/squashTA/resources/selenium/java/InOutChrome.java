57b3f3e010d43c3c8e3ef709b322fae1b97f1afe
invaderzim2 <invaderzim2@bigmir.net>
2015-06-01 18:26:50 +0300

    Ups! Sory

--- src/squashTA/resources/selenium/java/InOutChrome.java
+++ src/squashTA/resources/selenium/java/InOutChrome.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
+import java.io.File;
 
 public class InOutChrome {
   private WebDriver driver;
